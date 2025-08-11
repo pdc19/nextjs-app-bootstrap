1e202e72b8b73a41ba40195937a62dc91f259de1
pdc19 <85705271+pdc19@users.noreply.github.com>
2025-08-11 11:05:20 -0400

    Add .next/static/chunks/src_app_settings_page_tsx_53977b71._.js 1754924719927

---
+++ .next/static/chunks/src_app_settings_page_tsx_53977b71._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c88e6ebe._.js",
+  "static/chunks/src_f8c1b057._.js"
+],
+    source: "dynamic"
+});
