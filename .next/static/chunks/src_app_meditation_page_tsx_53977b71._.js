7e09bde53bb1859c73a914e2b6df46d1f2dfce59
pdc19 <85705271+pdc19@users.noreply.github.com>
2025-08-11 11:05:30 -0400

    Add .next/static/chunks/src_app_meditation_page_tsx_53977b71._.js 1754924730026

---
+++ .next/static/chunks/src_app_meditation_page_tsx_53977b71._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_80fb9916._.js",
+  "static/chunks/src_da804058._.js"
+],
+    source: "dynamic"
+});
