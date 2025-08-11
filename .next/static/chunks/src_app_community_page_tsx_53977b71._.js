1fc39e56c07e402e35ceca042be2ec3828fd6075
pdc19 <85705271+pdc19@users.noreply.github.com>
2025-08-11 11:05:23 -0400

    Add .next/static/chunks/src_app_community_page_tsx_53977b71._.js 1754924723310

---
+++ .next/static/chunks/src_app_community_page_tsx_53977b71._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b3c7a983._.js",
+  "static/chunks/src_af071ef9._.js"
+],
+    source: "dynamic"
+});
